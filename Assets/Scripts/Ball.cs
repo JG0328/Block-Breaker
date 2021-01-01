0bda70bce6560b7739b3d03f4cda33adf848a8f0
José Gabriel González <jgg032899@gmail.com>
2021-01-01 18:57:37 -0400

    Attaching the ball to the paddle

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+
+    Vector2 paddleToBallVector;
+    Rigidbody2D myRigidBody2D;
+
+    private void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myRigidBody2D.simulated = false;
+    }
+
+    private void Update()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    private void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
